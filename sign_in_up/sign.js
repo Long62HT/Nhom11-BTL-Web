6fdcd8f22de1c556bdf13fe90b40f9f94128859c
VanKhanhTran281 <tranvankhanh2812002@gmail.com>
2022-06-17 22:06:52 +0700

    sua sign_up

--- sign_in_up/sign.js
+++ sign_in_up/sign.js
@@ -1,5 +1,10 @@
 function signup(e) {
     event.preventDefault();
+    var arr = document.getElementsByTagName('input');
+    var name =arr[0].value;
+    var email  =arr[1].value;
+    var passw =arr[2].value;
+    var passw1 =arr[3].value;
     var username=document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
@@ -9,10 +14,16 @@ function signup(e) {
       email: email,
       password: password,
     };
+    if (name=="" || email==""|| passw=="" || passw1=="" ){
+      alert("Vui lòng điền đầy đủ thông tin")
+    }
+    else{
     data.push(user);
     var json = JSON.stringify(data);
     localStorage.setItem("data", json);
     alert("dang ky thanh cong");
+    }
+    
   }
   
   var user = localStorage.getItem("data");
