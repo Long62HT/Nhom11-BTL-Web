63ff1c1cb89f5b3783cb6ec1a37086e18f2afb2f
Duc Long <2051063665@e.tlu.edu.vn>
2022-06-21 22:50:55 +0700

    link thêm các trang

--- admin/admin-user.js
+++ admin/admin-user.js
@@ -15,11 +15,15 @@ const tBody = document.querySelector('tbody')
 const itemsUser = JSON.parse(localStorage.getItem('data'))
 console.log(itemsUser);
 
+
+  window.addEventListener('DOMContentLoaded',renderHtml(itemsUser))
+
+
 // Event luc load trang web lan dau
-window.addEventListener('DOMContentLoaded',renderHtml(itemsUser))
+
 
 // Ngan chan su kien reload khi submit form
-document.querySelector("form").addEventListener("submit",e=>{
+document.querySelector("form").addEventListener("submit",e =>{
     e.preventDefault();
 });
 
@@ -70,7 +74,7 @@ function renderHtml(data){
           </svg></button>  
         </td>
         <td>
-          <button>
+          <button class="delBtn">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
             </svg>
@@ -83,15 +87,24 @@ function renderHtml(data){
     tBody.innerHTML = htmls.join('')
 }
 
-const deleteBtn = document.querySelector('.bi-trash3');
 
-deleteBtn.addEventListener('click',(e)=>{
-    let item =(e.target.parentElement.parentElement.parentElement)
+const deleteBtn = document.querySelectorAll('.delBtn');
+
+
+// Xoa dữ liệu
+deleteBtn.forEach(e=>{
+  e.addEventListener('click',el=>{
+    console.log(e);
+    let item = e.parentElement.parentElement;
+    console.log(item);
+    // Lấy vị trí cần xóa
+    let index = item.querySelectorAll('td')[0];
    
     item.remove();
+    itemsUser.splice(index-1,1)
     saveItems(itemsUser)
-    renderHtml(itemsUser)
-    console.log(item);
+    // renderHtml(itemsUser)
+    
+  })
 })
-
 
